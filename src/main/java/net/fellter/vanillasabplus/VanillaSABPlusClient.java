035f7c64a38a82daccc586fdfd80c9b74577bcc9
Fellteros <aloisprochazka6@gmail.com>
2024-06-24 18:37:04 +0200

    1.0.0

---
+++ src/main/java/net/fellter/vanillasabplus/VanillaSABPlusClient.java
@@ -0,0 +1,10 @@
+package net.fellter.vanillasabplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaSABPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
